6a337fcde7ca2d371151862193d27d74f3cf3b09
sundasmustaf69 <sundasmustafa69@gmail.com>
2025-09-27 16:27:50 +0500

    task2.py

--- task2.py
+++ task2.py
@@ -31,7 +31,7 @@ st.write("Paste any **customer query** below and see key entities highlighted.")
 # -------------------------------
 # Step 2: User Input
 # -------------------------------
-user_input = st.text_area("✍️ Enter customer query:", "Do you have the iPhone 15 in stock?")
+user_input = st.text_area("✍️ Enter customer query:")
 
 if st.button("Extract Entities"):
     if user_input.strip():
@@ -62,5 +62,6 @@ if st.button("Extract Entities"):
             st.warning("No entities found. Try another query!")
     else:
         st.error("Please enter some text!")
+
 
 
